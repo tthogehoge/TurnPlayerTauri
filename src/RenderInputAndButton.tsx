d1c816db36a440686ffb4e2fb1ca4c8bc130766c
tthogehoge <tt.hogehoge@gmail.com>
2023-12-03 00:28:24 +0900

    フォルダ選択ボタン追加

--- src/RenderInputAndButton.tsx
+++ src/RenderInputAndButton.tsx
@@ -1,7 +1,9 @@
 // 新しいコンポーネントを作成
 import React from "react";
-import { Box, Input, Button } from "@mui/material";
+import { Box, Input, Button, IconButton } from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 import { SSetting } from "./App";
+import { open } from "@tauri-apps/api/dialog";
 
 type RenderInputAndButtonProps = {
   dir: string;
@@ -20,28 +22,57 @@ const RenderInputAndButton: React.FC<RenderInputAndButtonProps> = ({
   findFiles,
   getFiles,
 }) => {
+  // フォルダ選択ダイアログを出す関数
+  async function selectFolder(dir: string) {
+    // Open a selection dialog for image files
+    const selected = await open({
+      directory: true,
+      multiple: false,
+      defaultPath: dir,
+    });
+    if (Array.isArray(selected)) {
+      // user selected multiple files
+    } else if (selected === null) {
+      // user cancelled the selection
+    } else {
+      // user selected a single file
+      console.log(selected);
+      setDir(selected);
+    }
+  }
+
   return (
     <>
-      <Box>Search directory</Box>
-      <Input
-        id="dir-input"
-        onChange={(e:any) => setDir(e.currentTarget.value)}
-        placeholder="Enter a directory..."
-        value={dir}
-        fullWidth={true}
-      />
-      <Box>Search string</Box>
-      <Input
-        id="str-input"
-        onChange={(e:any) => setStr(e.currentTarget.value)}
-        placeholder="Enter a string..."
-        value={str}
-        fullWidth={true}
-      />
+      <Box display="flex" alignItems="center">
+        <Box>DIR: </Box>
+        <Input
+          id="dir-input"
+          onChange={(e: any) => setDir(e.currentTarget.value)}
+          placeholder="Enter a directory..."
+          value={dir}
+          fullWidth={true}
+        />
+        <IconButton
+          onClick={() => selectFolder(dir)}
+          aria-label="select folder"
+        >
+          <FolderIcon />
+        </IconButton>
+      </Box>
+      <Box display="flex" alignItems="center">
+        <Box>TEXT: </Box>
+        <Input
+          id="str-input"
+          onChange={(e: any) => setStr(e.currentTarget.value)}
+          placeholder="Enter a string..."
+          value={str}
+          fullWidth={true}
+        />
+      </Box>
       <Button
         variant="contained"
         type="submit"
-        onClick={(e:any) => {
+        onClick={(e: any) => {
           e.preventDefault();
           findFiles({ dir, str });
         }}
@@ -51,7 +82,7 @@ const RenderInputAndButton: React.FC<RenderInputAndButtonProps> = ({
       <Button
         variant="contained"
         type="submit"
-        onClick={(e:any) => {
+        onClick={(e: any) => {
           e.preventDefault();
           getFiles();
         }}
